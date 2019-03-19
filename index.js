3503446bc2ae8dc7b715b2df79d012a1fa826b6f
Ksu <yarks01@yandex.ru>
2019-03-19 23:56:08 +0300

    Some problem :(

---
+++ index.js
@@ -0,0 +1,13 @@
+// document.addEventListener("DOMContentLoaded", function(event){
+//  alert("Hello, everything is good!");
+// });
+
+// window.onload = function(){
+//   alert("Hello, everything is good!");
+// };
+
+// alert("Hello, everything is good!");
+
+document.addEventListener('DOMContentLoaded', function() {
+    alert("Ready!");
+}, false);
