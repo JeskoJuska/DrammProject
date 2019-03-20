e7917a3091f1e92f4d34ebba6392fcadbc36ffc8
Ksu <yarks01@yandex.ru>
2019-03-21 01:37:25 +0300

    Special for Artem

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+for(var i =0; i<document.querySelectorAll(".drum").length; i++){
+  document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+    var audio = new Audio('sounds/crash.mp3');
+    audio.play();
+  });
+}
+
+
+
+
+
+
+// problems with alert and font
+
 // document.addEventListener("DOMContentLoaded", function(event){
 //  alert("Hello, everything is good!");
 // });
@@ -8,6 +22,10 @@
 
 // alert("Hello, everything is good!");
 
-document.addEventListener('DOMContentLoaded', function() {
-    alert("Ready!");
-}, false);
+// document.addEventListener('DOMContentLoaded', function() {
+//     alert("Ready!");
+// }, false);
+
+// function scripts(){
+//   alert("Hello, everything is good!");
+// }
